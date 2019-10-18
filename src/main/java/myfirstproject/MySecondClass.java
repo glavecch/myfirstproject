132fe02594ff2eecbf02c0f7d6134a27d4bb8350
glavecch <giovanni.lavecchia@capgemini.com>
2019-10-18 14:31:38 +0200

    Second commit

---
+++ src/main/java/myfirstproject/MySecondClass.java
@@ -0,0 +1,14 @@
+package myfirstproject;
+
+public class MySecondClass {
+	private String myString;
+
+	public String getMyString() {
+		return myString;
+	}
+
+	public void setMyString(String myString) {
+		this.myString = myString;
+	}
+	
+}
